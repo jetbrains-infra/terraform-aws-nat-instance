4c5f3d5b6df32567d3f37718e11b3cbba045068d
Sergei Ugdyzhekov <sergey.ugdyzhekov@jetbrains.com>
2020-06-30 11:15:08 +0200

    Allow external DNS

--- security_group.tf
+++ security_group.tf
@@ -22,6 +22,24 @@ resource "aws_security_group_rule" "ingess_https" {
   security_group_id = aws_security_group.nat.id
 }
 
+resource "aws_security_group_rule" "ingess_dns_tcp" {
+  type              = "ingress"
+  from_port         = 53
+  protocol          = "tcp"
+  to_port           = 53
+  cidr_blocks       = local.private_subnet_cidrs
+  security_group_id = aws_security_group.nat.id
+}
+
+resource "aws_security_group_rule" "ingess_dns_udp" {
+  type              = "ingress"
+  from_port         = 53
+  protocol          = "udp"
+  to_port           = 53
+  cidr_blocks       = local.private_subnet_cidrs
+  security_group_id = aws_security_group.nat.id
+}
+
 resource "aws_security_group_rule" "egress_http" {
   type              = "egress"
   from_port         = 80
@@ -36,6 +54,24 @@ resource "aws_security_group_rule" "egress_https" {
   from_port         = 443
   protocol          = "tcp"
   to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.nat.id
+}
+
+resource "aws_security_group_rule" "egress_dns_tcp" {
+  type              = "egress"
+  from_port         = 53
+  protocol          = "tcp"
+  to_port           = 53
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.nat.id
+}
+
+resource "aws_security_group_rule" "egress_dns_udp" {
+  type              = "egress"
+  from_port         = 53
+  protocol          = "udp"
+  to_port           = 53
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.nat.id
 }
