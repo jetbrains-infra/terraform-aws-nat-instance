5a192bd2a9010275a0c67aa17217aa2dc6c555a9
Sergei Ugdyzhekov <sergei.ugdyzhekov@jetbrains.com>
2020-11-26 19:20:32 +0100

    fix: w/o of terraform https://github.com/hashicorp/terraform/pull/2376

--- security_group.tf
+++ security_group.tf
@@ -3,24 +3,24 @@ resource "aws_security_group" "nat" {
   description = "Rules for Nat instance"
   vpc_id      = local.vpc_id
   tags        = local.tags
-}
 
-resource "aws_security_group_rule" "ingress" {
-  count             = length(local.ports)
-  type              = "ingress"
-  from_port         = element(local.ports, count.index)
-  protocol          = "tcp"
-  to_port           = element(local.ports, count.index)
-  cidr_blocks       = local.private_subnet_cidrs
-  security_group_id = aws_security_group.nat.id
-}
+  dynamic "ingress" {
+    for_each = local.ports
+    content {
+      cidr_blocks = local.private_subnet_cidrs
+      from_port   = ingress.value
+      protocol    = "tcp"
+      to_port     = ingress.value
+    }
+  }
 
-resource "aws_security_group_rule" "egress" {
-  count             = length(local.ports)
-  type              = "egress"
-  from_port         = element(local.ports, count.index)
-  protocol          = "tcp"
-  to_port           = element(local.ports, count.index)
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.nat.id
+  dynamic "egress" {
+    for_each = local.ports
+    content {
+      cidr_blocks = ["0.0.0.0/0"]
+      from_port   = egress.value
+      protocol    = "tcp"
+      to_port     = egress.value
+    }
+  }
 }
